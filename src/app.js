51998fcce8f5e37c765dbaf01925ee2a29feab7b
jepthe <jepthe.hf@gmail.com>
2025-04-11 15:45:54 -0600

    años posición vertical desde la base de la celda, eliminando el ultimo

--- src/app.js
+++ src/app.js
@@ -518,15 +518,14 @@ app.get('/api/reportes/pdf', async (req, res) => {
               doc.save();
               
               // Mover el punto de origen al centro vertical y ligeramente a la derecha del borde izquierdo de la celda
-              // Posicionar en la parte inferior de la celda
-              doc.translate(xPos + (dataColWidth / 2), yPos + 25);
+              doc.translate(xHeader + 10, yPos + 15);
               
               // Rotar 90 grados en sentido anti-horario
               doc.rotate(-90);
               
               // Dibujar el texto centrado respecto al nuevo origen
-              // Dibujar el texto alineado al final
-              doc.fillColor('#000000').fontSize(8).text(year.toString(), -15, -4, { 
+              doc.fillColor('#000000').text(year.toString(), 0, 0, { 
+                width: 25, // Altura original de la celda, ahora es el ancho del texto rotado
                 align: 'center'
               });
               
